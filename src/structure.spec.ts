64b855c6a7206db80b8289f3df673be49808f182
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-05-07 00:22:05 +0200

    basic setup is in place

--- src/structure.spec.ts
+++ src/structure.spec.ts
@@ -17,7 +17,10 @@ describe('the business reason behind this test case', () => {
         //tag::arrange-act-assert[]
         it('the tax rate is the sum of the base tax and a variable market tax', () => {
             const server = jest.fn().mockReturnValue({ server: { response: 12 } });
-            let result = new Production(server).totalTaxWithBaseTax(7);
+            let production = new Production(server);
+
+            let result = production.totalTaxWithBaseTax(7);
+
             expect(result).toEqual(19);
         });
         //end::arrange-act-assert[]
@@ -34,8 +37,10 @@ describe('the business reason behind this test case', () => {
         //tag::collect-act-assert[]
         it('the tax rate is the sum of the base tax and a variable market tax', () => {
             marketTaxIs(12);
+
             expect(totalTaxRateForBaseRate(7)).toEqual(19);
         });
+
         //end::collect-act-assert[]
 
         function marketTaxIs(value: number) {
---
+++ src/transition.spec.js
@@ -0,0 +1,10 @@
+describe('transition', () => {
+  it('should be Batman', () => {
+    //tag::transition[]
+    Array(8).join('wat' - 1) + " Examples!"
+    //end::transition[]
+  });
+  it('should be Batman!', () => {
+    console.log(Array(8).join('wat' - 1) + " Examples!")
+  });
+});
